a52bf53a651f4fe3d2b056af4bf9dd36105219b7
chenlx92 <chenlx92@outlook.com>
2017-05-17 10:55:12 +0800

    init version

---
+++ src/emg_recorder.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+import rospy
+from myo_driver.msg import emg_ch
+
+
+def callback_emg(data):
+    global f_emg
+    f_emg.write(str(data.ch0)+' '+str(data.ch1)+' '+str(data.ch2)+' '+
+            str(data.ch3)+' '+str(data.ch4)+' '+str(data.ch5)+' '+
+            str(data.ch6)+' '+str(data.ch7)+' '+'\n')
+    rospy.loginfo("emg data recording")    
+
+
+if __name__ == '__main__':
+    
+    # ros node init
+    rospy.init_node('recorder_emg', anonymous=True)
+    
+    #emg file read directory
+    fileDir_emg = sys.path[0]+"/../datasets/emg_data0.txt"
+    f_emg=open(fileDir_emg,'w')
+    rospy.Subscriber("myo_pub", emg_ch, callback_emg)
+    
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
---
+++ src/marker_recorder.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys
+import rospy
+from geometry_msgs.msg import PoseStamped
+
+
+def callback_marker(data):
+    global f_marker
+    x_marker = data.pose.position.x
+    y_marker = data.pose.position.y
+    z_marker = data.pose.position.z
+    qx_marker = data.pose.orientation.x
+    qy_marker = data.pose.orientation.y
+    qz_marker = data.pose.orientation.z
+    qw_marker = data.pose.orientation.w
+    f_marker.write(str(x_marker)+' '+str(y_marker)+' '+str(z_marker)+' '+
+            str(qx_marker)+' '+str(qy_marker)+' '+str(qz_marker)+' '+
+            str(qw_marker)+' '+'\n')
+    rospy.loginfo("marker data recording")    
+
+
+if __name__ == '__main__':
+    
+    # ros node init
+    rospy.init_node('recorder_marker', anonymous=True)
+    
+    # maker file read directory
+    fileDir_marker = sys.path[0]+"/../datasets/marker_data0.txt"
+    f_marker=open(fileDir_marker,'w')
+    rospy.Subscriber("/aruco_tracker/pose", PoseStamped, callback_marker)
+    
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
