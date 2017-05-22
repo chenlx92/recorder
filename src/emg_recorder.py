5516368ecf9ea658089a2f720e36e8df511ff18e
chenlx92 <chenlx92@outlook.com>
2017-05-22 17:21:22 +0800

    change the topic name

--- src/emg_recorder.py
+++ src/emg_recorder.py
@@ -6,7 +6,8 @@ from myo_driver.msg import emg_ch
 
 def callback_emg(data):
     global f_emg
-    f_emg.write(str(data.ch0)+' '+str(data.ch1)+' '+str(data.ch2)+' '+
+    f_emg.write( str(data.header.stamp)+' '+
+            str(data.ch0)+' '+str(data.ch1)+' '+str(data.ch2)+' '+
             str(data.ch3)+' '+str(data.ch4)+' '+str(data.ch5)+' '+
             str(data.ch6)+' '+str(data.ch7)+' '+'\n')
     rospy.loginfo("emg data recording")    
@@ -20,7 +21,7 @@ if __name__ == '__main__':
     #emg file read directory
     fileDir_emg = sys.path[0]+"/../datasets/emg_data0.txt"
     f_emg=open(fileDir_emg,'w')
-    rospy.Subscriber("myo_pub", emg_ch, callback_emg)
+    rospy.Subscriber("myo_raw_pub", emg_ch, callback_emg)
     
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
